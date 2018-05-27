ede2502c25535a12e533598bdd066801acea0f96
Tang Ziya <tcztzy@gmail.com>
2018-05-27 22:46:53 +0800

    FooterDisplay & PrivacyDisplay

---
+++ src/components/FooterDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <ul class="nav nav-tabs">
+                <li v-for="f of footer" :key="f" class="nav-item" @click="selectPane(f)">
+                    <a :class="['nav-link', state.selected ? 'active' : '', state.notified ? 'item-notified' : '']" :href="`#${f.id}`">{{ f.name }}</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+    name: 'footer-display',
+    components: {},
+    computed: mapGetters(['research', 'panes']),
+    methods: mapActions(['selectPane'])
+};
+</script>
---
+++ src/components/PrivacyDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>Privacy Policy</h3>
+        We do not collect or store any of your personal information.
+        <br>
+        However, we use PlayFab and Google Analytics for internal usage analysis.
+        <br>
+        We use PlayFab analytics to guide game improvements.
+        <br>
+        Google Analytics uses cookies to store non-personally identifiable information.
+        <br>
+        For more information, see <a href="https://support.google.com/analytics/answer/6004245?hl=en">https://support.google.com/analytics/answer/6004245?hl=en</a>.
+        <br>
+        If you do not agree to the use of Google Analytics, please download a browser plugin to block Google Analytics.
+        <br>
+    </div>
+</template>
+<script>
+export default {
+    name: 'privacy-display'
+};
+</script>
