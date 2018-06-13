e622191049855e855714578288df536c0ac39d7f
Tang Ziya <tcztzy@gmail.com>
2018-06-13 16:15:48 +0800

    not defined

--- src/lib/research/computer-science/software-engineering.js
+++ src/lib/research/computer-science/software-engineering.js
@@ -1,16 +1,16 @@
-import { createCompletedOrReadingListLock, createLevelLock } from "../../locks";
-import { LevelAddMultModifier } from "../../modifiers/modifier";
+import { createCompletedOrReadingListLock, createLevelLock } from "@/lib/locks";
+import { LevelAddMultModifier } from "@/lib/modifiers/modifier";
 import { Area } from "../area";
 import { Book } from "../book";
 import { programming } from "./programming";
-import { softwareEngineer } from "../../careers/yugle";
+import { softwareEngineer } from "@/lib/careers/yugle";
 
 
-export let se1 = exports.se1 = new Book('se1', 'The Pragmatic Programmer', 'Tales from the development frontlines.', 150, '020161622X'),
-    se2 = exports.se2 = new Book('se2', 'Clean Code', 'Write the shiniest lines.', 200, '0132350882'),
-    se3 = exports.se3 = new Book('se3', 'Code Complete', 'If your code is not complete, you', 200, '0735619670'),
-    se4 = exports.se4 = new Book('se4', 'Design Patterns', 'Don\'t reinvent the wheel. You will probably make it square-shaped.', 250, '0201633612'),
-    se5 = exports.se5 = new Book('se5', 'Working with Legacy Code', 'Boooring. Or is it?', 250, '0131177052');
+export let se1 = new Book('se1', 'The Pragmatic Programmer', 'Tales from the development frontlines.', 150, '020161622X'),
+    se2 = new Book('se2', 'Clean Code', 'Write the shiniest lines.', 200, '0132350882'),
+    se3 = new Book('se3', 'Code Complete', 'If your code is not complete, you', 200, '0735619670'),
+    se4 = new Book('se4', 'Design Patterns', 'Don\'t reinvent the wheel. You will probably make it square-shaped.', 250, '0201633612'),
+    se5 = new Book('se5', 'Working with Legacy Code', 'Boooring. Or is it?', 250, '0131177052');
 createCompletedOrReadingListLock(se1, se2);
 createCompletedOrReadingListLock(se2, se3);
 createCompletedOrReadingListLock(se3, se4);
